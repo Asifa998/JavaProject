bec0caba099c41ff56c95abfd8b96e5d1281487e
asifa <asifahussains786@gmail.com>
2024-07-30 10:49:38 +0530

    FIRST COMMIT

---
+++ GitProject/src/GitPackage/GitClass.java
@@ -0,0 +1,13 @@
+package GitPackage;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		int a = 30;
+		int b = 40;
+		System.out.println("Sum="+(a+b));
+		// TODO Auto-generated method stub
+
+	}
+
+}
